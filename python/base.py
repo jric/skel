17f59fba4b0ee4c471618086301d62d28a05a915
Joshua Richardson <jricjr3@gmail.com>
2021-10-06 20:24:51 -0700

    A basic python module.

---
+++ python/base.py
@@ -0,0 +1,31 @@
+import unittest
+import apperror
+from docopt import docopt
+
+'''
+REPLACE DESCRIPTION OF THE MODULE OR PROGRAM HERE
+'''
+
+usage = """
+Usage:
+  REPLACE.py [--verbose]... [--debug]
+  REPLACE.py -h | --help
+  REPLACE.py --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+  -v --verbose  Show more diagnostics
+"""
+
+l = apperror.AppLogger('REPLACE.py')
+
+class TestREPLACE(unittest.TestCase):
+
+    def test_REPLACE(self):
+        pass
+
+if __name__ == '__main__':
+    args = docopt(usage, version='REPLACE.py 1.0')
+    l.setFromArgs(args)
+    unittest.main() 
